c32b1f5c0004f5d58ccd2d188083ee80f1a7ac2e
ZhangYu <18302112629@163.com>
2019-11-25 14:18:58 +0800

    first commit

---
+++ src/main/java/com/zhyu/cpmshiro/CpmshiroApplication.java
@@ -0,0 +1,13 @@
+package com.zhyu.cpmshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmshiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmshiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhyu/cpmshiro/CpmshiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhyu.cpmshiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmshiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
